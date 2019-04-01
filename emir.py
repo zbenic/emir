acc0a2352b3e51d6886021e14e0149f540f5d137
Jarvis Pennyworth <zbenic47250@gmail.com>
2019-04-01 23:38:18 +0200

    Added class documentation and minor refactoring done.

--- emir.py
+++ emir.py
@@ -6,8 +6,22 @@ warnings.simplefilter('always', UserWarning)
 
 VectorInt = [int]
 
+
 class Emir:
-    def __init__(self, name, numOfProximitySensors=6):
+    """
+    Class for FMENA eMIR robot.
+    Implemented methods can be used to achieve basic robot movement.
+    Implementation of parsing of the return status message can also be used.
+    """
+
+    def __init__(self, name: str, numOfProximitySensors: int=6):
+        """
+        Init method (can be looked at as a constructor).
+
+        :param name: Name of the robot instance
+        :param numOfProximitySensors: Number of the proximity sensors (default is 6)
+        """
+
         self.name = name
         self.address = None
         self.numberOfRetries = 5
@@ -54,6 +68,14 @@ class Emir:
         self.badCheckSum = 0
 
     def __getRobotAddress(self):
+        """
+        Private method.
+        Retrieves the bluetooth address from the robot whose bluetooth device is the same name as the self.name.
+        The address is saved in the self.address.
+
+        :return: N/A
+        """
+
         retryNumber = 0
         while self.address is None and retryNumber < self.numberOfRetries:
             nearbyDevices = bluetooth.discover_devices()
@@ -67,6 +89,13 @@ class Emir:
                 retryNumber += 1
 
     def __getRobotPort(self):
+        """
+        Private method.
+        Updates self.port variable with one of the three eMIR robot port numbers, depending on the instance name.
+
+        :return: N/A
+        """
+
         if self.name == "eMIR-Yellow":
             self.port = 27
         elif self.name == "eMIR-Blue":
@@ -77,14 +106,30 @@ class Emir:
             self.port = None
 
     def __getCheckSum(self, *args: int):
+        """
+        Private method.
+        Returns sum(args) mod 2^8+1 (max. 8bit number + 1).
+
+        :param args: Variable number of integer arguments
+        :return: Checksum
+        """
+
         argumentSum = 0
         for arg in args:
             argumentSum += arg
 
-        return ((self.__getMax8bitIntegerValue() + 1) - argumentSum % (self.__getMax8bitIntegerValue() + 1)) % (self.__getMax8bitIntegerValue() + 1)
+        return ((self.getMax8bitIntegerValue() + 1) - argumentSum % (self.getMax8bitIntegerValue() + 1)) % (self.getMax8bitIntegerValue() + 1)
 
     @staticmethod
-    def __getTwosComplement8bit(decimalBaseInteger: int):
+    def getTwosComplement8bit(decimalBaseInteger: int):
+        """
+        Static method.
+        Returns 8bit two's complement.
+
+        :param decimalBaseInteger: Integer number in decimal base
+        :return: 8bit two's complement
+        """
+
         if (decimalBaseInteger.bit_length() > 8) or (-128 > decimalBaseInteger or decimalBaseInteger > 127):
             warnings.warn("The number does not fit in 8 bits or it's outside of [-128, 127] interval!")
             return None
@@ -92,15 +137,39 @@ class Emir:
             return decimalBaseInteger if decimalBaseInteger >= 0 else 256 + decimalBaseInteger
 
     @staticmethod
-    def __clipValue(value, minValue, maxValue):
+    def clipValue(value, minValue, maxValue):
+        """
+        Static method.
+        Returns value clipped by the min and max limits
+
+        :param value: Input value
+        :param minValue: Lower clipping bound
+        :param maxValue: Upper clipping bound
+        :return: Clipped input value
+        """
+
         return max(minValue, min(value, maxValue))
 
     @staticmethod
-    def __getMax8bitIntegerValue():
+    def getMax8bitIntegerValue():
+        """
+        Static method.
+        Returns max. 8bit integer number.
+
+        :return: Max. 8bit integer number
+        """
+
         return 2**8 - 1
 
     @staticmethod
-    def __stringOfHexToListOfDecIntegers(string: str):
+    def stringOfHexToListOfDecIntegers(string: str):
+        """
+        Converts string to list of integers.
+
+        :param string: Input string
+        :return: List of integers in decimal base
+        """
+
         groupSize = 2  # 2 strings in each group
         listOfHexStrings = [string[i:i + groupSize] for i in range(0, len(string), groupSize)]
         listOfDecimalIntegers = [int(hexStr, 16) for hexStr in listOfHexStrings]
@@ -108,12 +177,20 @@ class Emir:
         return listOfDecimalIntegers
 
     def __messageIsValid(self, *args):
+        """
+        Private method.
+        Checks if incoming or outgoing message is valid, i.e. if the checksum is equal to zero.
+
+        :param args: Variable number of integer arguments
+        :return: Information if the message is valid
+        """
+
         if isinstance(args, Iterable):
             argumentSum = sum(args[0])
         else:
             argumentSum = sum(args)
 
-        if argumentSum % (self.__getMax8bitIntegerValue() + 1) == 0:
+        if argumentSum % (self.getMax8bitIntegerValue() + 1) == 0:
             return True
         else:
             self.badCheckSum += 1
@@ -129,24 +206,25 @@ class Emir:
                       useValueClipping: bool = True,
                       verbose: bool = True):
         """
+        Private method.
         Generic method for sending commands to the robot.
         Command format is #NNaabbCS/, where:
-        #  - message start
-        NN - command number
-        aa - first parameter
-        bb - second parameter
-        CS - checksum (NN+aa+bb+CS=0)
-        /  - message end
+            #  - message start
+            NN - command number
+            aa - first parameter
+            bb - second parameter
+            CS - checksum (NN+aa+bb+CS=0)
+            /  - message end
 
-        :param commandName:
-        :param firstArg:
-        :param secondArg:
-        :param firstArgLimits:
-        :param secondArgLimits:
-        :param useTwosComplement:
-        :param useValueClipping:
-        :param verbose:
-        :return:
+        :param commandName: The name of the command (move, translate, ...)
+        :param firstArg: The first argument of the robot command
+        :param secondArg: The second argument of the robot command
+        :param firstArgLimits: First argument's numeric limits
+        :param secondArgLimits: Second argument's numeric limits
+        :param useTwosComplement: Flag for using two's complement
+        :param useValueClipping: Flag for using value clipping
+        :param verbose: If used, commands are also printed to the console
+        :return: N/A  # TODO: return bool if the command is successfully sent.
         """
 
         # get the command number from command name
@@ -155,13 +233,13 @@ class Emir:
 
         # value clipping
         if useValueClipping:
-            firstArg = self.__clipValue(firstArg, firstArgLimits[0], firstArgLimits[1])
-            secondArg = self.__clipValue(secondArg, secondArgLimits[0], secondArgLimits[1])
+            firstArg = self.clipValue(firstArg, firstArgLimits[0], firstArgLimits[1])
+            secondArg = self.clipValue(secondArg, secondArgLimits[0], secondArgLimits[1])
 
         # calculate 8 bit two's complement
         if useTwosComplement:
-            firstArg = self.__getTwosComplement8bit(firstArg)
-            secondArg = self.__getTwosComplement8bit(secondArg)
+            firstArg = self.getTwosComplement8bit(firstArg)
+            secondArg = self.getTwosComplement8bit(secondArg)
 
         # calculate checksum parameter of the command
         checkSum = self.__getCheckSum(commandNumberInt, firstArg, secondArg)
@@ -183,13 +261,21 @@ class Emir:
             warnings.warn("Command '" + commandName + "': checksum is not 0!")
 
     def __parseStatusMessage(self):
+        """
+        Private method.
+        Parses received robot's status message.
+        Updates corresponding variables with received data (self.battery, self.speed, self.angle, ...).
+
+        :return: N/A
+        """
+
         statusMessage = self.statusMessage.decode('utf-8')
         if statusMessage[0] != '*':
             warnings.warn("Problem occurred with parsing the robot status message. First character is not '*', but " + statusMessage[0] + ".")
         if statusMessage[-1] != '/':
             warnings.warn("Problem occurred with parsing the robot status message. Last character is not '/', but " + statusMessage[-1] + ".")
 
-        statusMessageParts = self.__stringOfHexToListOfDecIntegers(statusMessage[1:-1])
+        statusMessageParts = self.stringOfHexToListOfDecIntegers(statusMessage[1:-1])
 
         if not self.__messageIsValid(statusMessageParts):
             warnings.warn("Message '" + statusMessage + "' invalid: checksum is not 0!")
@@ -249,6 +335,14 @@ class Emir:
         self.angle = int(messageAngleValueInt * 2.14) if messageAngleValueInt < 128 else int((messageAngleValueInt - 256) * 2.14)
 
     def connect(self):
+        """
+        Connects with the bluetooth module on the eMIR robot.
+        If everything goes fine, self.sock is updated with the instance of created socket.
+        If unable to connect, program exits with -1.
+
+        :return: N/A
+        """
+
         self.__getRobotAddress()
         self.__getRobotPort()
 
@@ -262,7 +356,29 @@ class Emir:
             exit(-1)
 
     def getRobotStatus(self):
-        # self.statusString = self.sock.recv(128)  #TODO: check if 64bytes is enough
+        """
+        Receives and parses robot status message.
+        If everything goes fine, self.update is set to True, else is False.
+        The message format is *aabbccddeeffuuvvrrgghhmmjjkkppssCS/, where:
+            *      - message start
+            aa..ff - SHARP proximity sensors [cm]
+            uu     - battery or charging voltage (MSB=0, or MSB=1, respectively) [V]
+            vv     - translation speed [%]
+            rr     - rotation speed [%]
+            gg     - left motor PWM [%]
+            hh     - right motor PWM [%]
+            mm     - work mode (see eMIR documentation)
+            jj     - digital inputs status
+            kk     - azimuth [deg]
+            pp     - path [cm]
+            ss     - angle [deg/2]
+            CS     - checksum (aa+bb+..+ss+CS=0)
+            /      - message end
+
+        :return: N/A
+        """
+        self.statusMessage = None
+        self.statusMessage = self.sock.recv(128)  # TODO: check if 64bytes is enough
         messageStart = self.statusMessage.find(b'*')
         messageEnd = messageStart + self.statusMessage[messageStart:].find(b'/') + 1
         self.statusMessage = self.statusMessage[messageStart:messageEnd]
@@ -275,21 +391,20 @@ class Emir:
 
     def stop(self):
         """
+        Sends command to stop the robot in the current execution.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('stop')
 
     def move(self, translationSpeed: int, rotationSpeed: int):
         """
-        Move robot with defined translation and rotational speed
+        Sends "move" command to the robot with defined translation and rotational speed.
 
-        :param translationSpeed: translation speed
-        :param rotationSpeed: rotation speed
+        :param translationSpeed: Translation speed [%]
+        :param rotationSpeed: Rotation speed [%]
         :return: N/A
-
         """
 
         firstArgLimits = [-100, 100]  # [%]
@@ -299,11 +414,11 @@ class Emir:
 
     def translate(self, path: int, speed: int):
         """
+        Sends "translate" command to the robot with defined path and translation speed.
 
-        :param path:
-        :param speed:
-        :return:
-
+        :param path: Length for robot to travel [cm]
+        :param speed: Translation speed [%]
+        :return: N/A
         """
 
         firstArgLimits = [-100, 100]  # [cm]
@@ -313,11 +428,11 @@ class Emir:
 
     def rotate(self, angle: int, speed: int):
         """
+        Sends "rotate" command to the robot with defined angle and rotation speed.
 
-        :param angle:
-        :param speed:
-        :return:
-
+        :param angle: Robot rotation angle [deg]
+        :param speed: Rotation speed [%]
+        :return: N/A
         """
 
         firstArgLimits = [-180, 180]  # [deg]
@@ -329,34 +444,34 @@ class Emir:
 
     def setMinDistance(self, distance: int):
         """
+        Sends command to the robot which sets minimal allowed distance between robot and obstacle.
 
-        :param distance:
-        :return:
-
+        :param distance: Minimal distance between the robot and obstacle [cm]
+        :return: N/A
         """
 
         firstArgLimits = [0, 50]  # [cm]
 
         self.__sendCommand('setMinDistance', distance, firstArgLimits=firstArgLimits)
 
-    def setMaxSpeed(self, translationSpeed):
+    def setMaxSpeed(self, translationSpeed: int):
         """
+        Sends command to the robot which sets maximal translation speed.
 
-        :param translationSpeed:
-        :return:
-
+        :param translationSpeed: Maximal robot translation speed [%]
+        :return: N/A
         """
 
         firstArgLimits = [10, 100]  # [%]
 
         self.__sendCommand('setMaxSpeed', translationSpeed, firstArgLimits=firstArgLimits)
 
-    def setMaxRotation(self, rotationSpeed):
+    def setMaxRotation(self, rotationSpeed: int):
         """
+        Sends command to the robot which sets maximal rotation speed.
 
-        :param rotationSpeed:
-        :return:
-
+        :param rotationSpeed: Maximal robot rotation speed [%]
+        :return: N/A
         """
 
         firstArgLimits = [10, 100]  # [%]
@@ -365,29 +480,29 @@ class Emir:
 
     def resetAzimuth(self):
         """
+        Sends command to the robot which sets azimuth value to zero.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('resetAzimuth')
 
     def resetCounters(self):
         """
+        Sends command to the robot which sets left and right wheel encoder counters to zero.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('resetCounters')
 
-    def setDigitalOutput(self, bitNumber, state):
+    def setDigitalOutput(self, bitNumber: int, state: int):
         """
+        Sends command which sets digital inputs defined with the bitNumber argument to the desired state
 
-        :param bitNumber:
-        :param state:
-        :return:
-
+        :param bitNumber: Number of the bit (0, 1, 2, 3). If > 3, state argument sets multiple bits.
+        :param state: State of the bit defined with bitNumber.
+        :return: N/A
         """
 
         firstArgLimits = [0, 4]
@@ -399,12 +514,12 @@ class Emir:
 
         self.__sendCommand('setDigitalOutput', bitNumber, state, firstArgLimits, secondArgLimits)
 
-    def beep(self, duration):
+    def beep(self, duration: int):
         """
+        Sends command which activates the horn for the defined duration.
 
-        :param duration:
-        :return:
-
+        :param duration: Duration for which to activate the horn (duration/10 s)
+        :return: N/A
         """
 
         firstArgLimits = [0, 255]  # [1/10s]
@@ -413,54 +528,54 @@ class Emir:
 
     def sendInfoEEPROM(self):
         """
+        Sends command which enables the EEPROM data to be returned via status message.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('sendInfoEEPROM', 240, useTwosComplement=False, useValueClipping=False)  # 240 is F0
 
     def sendInfoOn(self):
         """
+        Sends command which enables the robot to send status messages.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('sendInfoOn', 1, useValueClipping=False)  # 240 is F0
 
     def sendInfoOff(self):
         """
+        Sends command which disables the robot to send status messages.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('sendInfoOff', 0, useValueClipping=False)  # 240 is F0
 
     def sensorsOn(self):
         """
+        Sends command which enables robot's sensors.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('sensorsOn', 1, useValueClipping=False)
 
     def sensorsOff(self):
         """
+        Sends command which disables robot's sensors.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('sensorsOff', 1, useValueClipping=False)
 
     def turnOff(self):
         """
+        Sends command to turn off the robot.
 
-        :return:
-
+        :return: N/A
         """
 
         self.__sendCommand('turnOff')
